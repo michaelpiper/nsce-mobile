d305cdaeaf5beec36ce22d2ad53763cb2c7b796a
michael-piper <56345637+michael-piper@users.noreply.github.com>
2020-02-06 14:21:46 +0100

    Update endpoints.dart

--- lib/services/endpoints.dart
+++ lib/services/endpoints.dart
@@ -1,7 +1,7 @@
-//const BASE_URL="https://www.nsce.com.ng";
-//const API_URL ="https://api.nsce.com.ng/v1";
-const BASE_URL="http://192.168.137.71:8090";
-const API_URL ="http://192.168.137.71:8090/api/v1";
+const BASE_URL="https://www.nsce.com.ng";
+const API_URL ="https://api.nsce.com.ng/v1";
+//const BASE_URL="http://192.168.137.71:8090";
+//const API_URL ="http://192.168.137.71:8090/api/v1";
 //const BASE_URL="http://127.0.0.1:8090";
 //const API_URL ="http://127.0.0.1:8090/api/v1";
 const API_AUTH_URL=API_URL+"/auth";
