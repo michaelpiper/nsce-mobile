43ca3316cbfb7aeacb08068c106a103a1fba1a3a
michael-piper <56345637+michael-piper@users.noreply.github.com>
2020-02-06 14:21:12 +0100

    Update endpoints.dart

--- lib/services/endpoints.dart
+++ lib/services/endpoints.dart
@@ -1,5 +1,5 @@
-//const BASE_URL="https://nsce-api.herokuapp.com";
-//const API_URL ="https://nsce-api.herokuapp.com/api/v1";
+//const BASE_URL="https://www.nsce.com.ng";
+//const API_URL ="https://api.nsce.com.ng/v1";
 const BASE_URL="http://192.168.137.71:8090";
 const API_URL ="http://192.168.137.71:8090/api/v1";
 //const BASE_URL="http://127.0.0.1:8090";
